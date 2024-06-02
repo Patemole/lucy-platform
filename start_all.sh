35e48bd2c6a13b540a756082f67980f84ba91245
Patemole <77934579+Patemole@users.noreply.github.com>
2024-06-02 19:01:02 +1000

    Changement et utilisation de nixpacks

--- start_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Démarrer chaque environnement en arrière-plan
-npm run start:upenn &
-npm run start:harvard &
-npm run start:usyd &
-
-# Attendre que tous les processus enfants se terminent
-wait
