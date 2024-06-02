67bd06b4b503d4d9bf9a6439cbda5a7cc8f6c5cc
Patemole <77934579+Patemole@users.noreply.github.com>
2024-06-02 15:30:37 +1000

    Modification du fichier config.js une deuxième fois

--- web/test_app_modulaire_socratic/src/config.js
+++ web/test_app_modulaire_socratic/src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
-
 const config = {
-    node_env: 'development', // ou 'production'
-    server_host: 'http://localhost',
-    server_port: 8000
+    node_env: process.env.REACT_APP_NODE_ENV || 'development',
+    server_host: process.env.REACT_APP_SERVER_HOST || 'http://localhost',
+    server_port: process.env.REACT_APP_SERVER_PORT || 8000
 };
 
 export default config;
+
